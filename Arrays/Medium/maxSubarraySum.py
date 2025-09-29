34ab0b6b2fed49020c5ca1b15dbdcfd630f2996c
Anuraag03 <f20211887@hyderabad.bits-pilani.ac.in>
2025-09-29 10:55:36 +0530

    Added a new variation for maxSubarraySum

--- Arrays/Medium/maxSubarraySum.py
+++ Arrays/Medium/maxSubarraySum.py
@@ -1,11 +1,30 @@
 def kadanesAlgo(nums):
     maxSum = nums[0]
     currSum = nums[0]
-    for i in range(1,len(nums)):
-        currSum+=nums[i]
-        maxSum=max(maxSum,currSum)
-        if currSum<0:
-            currSum = 0
+    for i in range(1, len(nums)):
+        currSum = max(nums[i], currSum + nums[i])  # either start fresh or extend
+        maxSum = max(maxSum, currSum)
     return maxSum
 
-print(kadanesAlgo([-3, -5, -2])) 
+def maxSumSubarray(nums):
+    maxSum = nums[0]
+    currSum = nums[0]
+    start = 0
+    temp = 0
+    end = 0
+    for i in range(1,len(nums)):
+        if nums[i]>currSum+nums[i]:
+            currSum = nums[i]
+            tempStart  = i
+        else:
+            currSum+=nums[i]
+        if currSum>maxSum:
+            start = tempStart
+            end = i
+            maxSum = currSum
+    return nums[start:end+1]
+
+
+nums = [-3, -5, -2, 1, 1, 1]
+print(kadanesAlgo(nums)) 
+print(maxSumSubarray(nums))
