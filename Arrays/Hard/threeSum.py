162430a0cd99c714f07ac6d9c04acc9f5616a65b
Anuraag03 <f20211887@hyderabad.bits-pilani.ac.in>
2025-09-29 13:23:38 +0530

    Added variation of 3sum

--- Arrays/Hard/threeSum.py
+++ Arrays/Hard/threeSum.py
@@ -22,3 +22,34 @@ def threeSum(nums,k):
                 high-=1
     return res
 
+def threeSumIndex(nums,k):
+    res = []
+    numsWithIndex = [(num,i) for i,num in enumerate(nums) ]
+    numsWithIndex.sort(key=lambda x: x[0])
+
+    for i in range(len(numsWithIndex)):
+        if i>0 and numsWithIndex[i][0] == numsWithIndex[i-1][0]:
+            continue
+        l = i+1
+        r = len(numsWithIndex)-1
+        while l<r:
+            currSum = numsWithIndex[i][0]+numsWithIndex[l][0]+numsWithIndex[r][0]
+            if currSum<k:
+                l+=1
+            elif currSum>k:
+                r-=1
+            else:
+                res.append([numsWithIndex[i][1],numsWithIndex[l][1],numsWithIndex[r][1]])
+                while l<r and numsWithIndex[l][0] == numsWithIndex[l+1][0]:
+                    l+=1
+                while l<r and numsWithIndex[r][0] == numsWithIndex[r-1][0]:
+                    r-=1
+                l+=1
+                r-=1
+    return res 
+
+
+
+nums = [-1,0,1,2,-1,-4]
+#print(threeSum(nums,0))
+print(threeSumIndex(nums,0))
